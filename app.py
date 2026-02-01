755f4e4f428df18706e67de07835a4e3d47a6340
Shreyas-9900 <PES2UG23CS564@pesu.pes.edu>
2026-02-01 16:21:00 +0530

    Changed Flask port to 8080

--- app.py
+++ app.py
@@ -145,6 +145,6 @@ def chat():
 
 # Run Server
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=8080)
+
 
